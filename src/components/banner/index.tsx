a641ec20cdf9d560215f83c441af10bac7ace309
Wesley Bruno <engwesleybruno@gmail.com>
2023-06-05 17:34:24 -0300

    feat: animation

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,18 +1,22 @@
+import { Fade } from "react-reveal";
+
 import { ContentType } from "../../hooks/types";
 
 export default function Banner({ acf }: ContentType) {
   return (
-    <section className="bg-white py-16">
-      <div
-        dangerouslySetInnerHTML={{ __html: acf?.titulo_secao_2 || "" }}
-        className="text-center capitalize text-dark_blue"
-      ></div>
+    <Fade bottom>
+      <section className="bg-white py-16">
+        <div
+          dangerouslySetInnerHTML={{ __html: acf?.titulo_secao_2 || "" }}
+          className="text-center capitalize text-dark_blue"
+        ></div>
 
-      <div className="flex justify-center gap-10 mt-12 flex-wrap">
-        {acf?.imagens_formulacao.map((img) => (
-          <img src={img.imagem.sizes.large} width={200} />
-        ))}
-      </div>
-    </section>
+        <div className="flex justify-center gap-10 mt-12 flex-wrap">
+          {acf?.imagens_formulacao.map((img) => (
+            <img src={img.imagem.sizes.large} width={200} />
+          ))}
+        </div>
+      </section>
+    </Fade>
   );
 }
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,30 +1,38 @@
+import { Fade } from "react-reveal";
+
 import { ContentType } from "../../hooks/types";
 
 export default function Main({ acf, title }: ContentType) {
   return (
     <main>
-      <div className="my-16 flex items-center gap-4 font-lato">
-        <div className="h-4 sm:w-20 w-10 bg-dark_blue rounded-tr-full rounded-br-full"></div>
-        <div className="h-4 w-4 bg-dark_green rounded-full"></div>
-        <h1 className="text-dark_gray uppercase">{title?.rendered}</h1>
-      </div>
+      <Fade bottom>
+        <div className="my-16 flex items-center gap-4 font-lato">
+          <div className="h-4 sm:w-20 w-10 bg-dark_blue rounded-tr-full rounded-br-full"></div>
+          <div className="h-4 w-4 bg-dark_green rounded-full"></div>
+          <h1 className="text-dark_gray uppercase">{title?.rendered}</h1>
+        </div>
+      </Fade>
 
-      <div className="min-h-32 bg-dark_blue flex p-4 sm:px-40 px-10 items-center">
-        <div
-          className="text-white font-lato font-light mr-[10%] leading-7"
-          dangerouslySetInnerHTML={{ __html: acf?.texto_banner_azul || "" }}
-        ></div>
-        <img
-          src={acf?.icone_banner_azul.url}
-          className="h-[100%] object-contain"
-        />
-      </div>
+      <Fade bottom>
+        <div className="min-h-32 bg-dark_blue flex p-4 sm:px-40 px-10 items-center">
+          <div
+            className="text-white font-lato font-light mr-[10%] leading-7"
+            dangerouslySetInnerHTML={{ __html: acf?.texto_banner_azul || "" }}
+          ></div>
+          <img
+            src={acf?.icone_banner_azul.url}
+            className="h-[100%] object-contain"
+          />
+        </div>
+      </Fade>
 
-      <div className="sm:px-40 px-10 my-16 font-lato text-dark_gray">
-        <div
-          dangerouslySetInnerHTML={{ __html: acf?.primeiro_conteudo || "" }}
-        ></div>
-      </div>
+      <Fade bottom>
+        <div className="sm:px-40 px-10 my-16 font-lato text-dark_gray">
+          <div
+            dangerouslySetInnerHTML={{ __html: acf?.primeiro_conteudo || "" }}
+          ></div>
+        </div>
+      </Fade>
     </main>
   );
 }
