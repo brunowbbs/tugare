dcee9ca8eefad661c39d97151c58f6c2dbb55a58
Wesley Bruno <engwesleybruno@gmail.com>
2023-06-05 17:35:31 -0300

    feat: animation

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -4,19 +4,18 @@ import { ContentType } from "../../hooks/types";
 
 export default function Banner({ acf }: ContentType) {
   return (
-    <Fade bottom>
-      <section className="bg-white py-16">
-        <div
-          dangerouslySetInnerHTML={{ __html: acf?.titulo_secao_2 || "" }}
-          className="text-center capitalize text-dark_blue"
-        ></div>
-
+    <section className="bg-white py-16">
+      <div
+        dangerouslySetInnerHTML={{ __html: acf?.titulo_secao_2 || "" }}
+        className="text-center capitalize text-dark_blue"
+      ></div>
+      <Fade bottom>
         <div className="flex justify-center gap-10 mt-12 flex-wrap">
           {acf?.imagens_formulacao.map((img) => (
             <img src={img.imagem.sizes.large} width={200} />
           ))}
         </div>
-      </section>
-    </Fade>
+      </Fade>
+    </section>
   );
 }
